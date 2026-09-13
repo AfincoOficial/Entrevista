17c081fc1474641d74ecb31975d6b1923a566172
AfincoOficial <nascimentoafinco@gmail.com>
2026-09-13 19:09:44 -0300

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,23 @@
+package com.afinco.alphacrm
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+class MainActivity : Activity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val web = WebView(this)
+        web.settings.javaScriptEnabled = true
+        web.settings.domStorageEnabled = true
+        web.settings.databaseEnabled = true
+        web.settings.allowFileAccess = false
+        web.settings.allowContentAccess = false
+        web.webViewClient = WebViewClient()
+        web.loadUrl("https://afincooficial.github.io/Entrevista/admin.html")
+        setContentView(web)
+    }
+}
